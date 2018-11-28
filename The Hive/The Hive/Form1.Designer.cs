87c9b6c110d5a2c16e727931b3d4f1b25c5ea22e
Mike sifanele <Mike.Sifanele2.0@gmail.com>
2018-11-28 12:07:02 +0200

    Add files via upload

---
+++ The Hive/The Hive/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace The_Hive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.dgvDisplay = new System.Windows.Forms.DataGridView();
+            this.colDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colStartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLeavingTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHoursSpent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.rbWatchFolder = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.AllowDrop = true;
+            this.btnImport.AutoSize = true;
+            this.btnImport.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnImport.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnImport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImport.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnImport.FlatAppearance.BorderSize = 2;
+            this.btnImport.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImport.ForeColor = System.Drawing.Color.Indigo;
+            this.btnImport.Location = new System.Drawing.Point(3, 3);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(118, 47);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            this.btnImport.DragDrop += new System.Windows.Forms.DragEventHandler(this.btnImport_DragDrop);
+            this.btnImport.DragEnter += new System.Windows.Forms.DragEventHandler(this.btnImport_DragEnter);
+            // 
+            // btnExport
+            // 
+            this.btnExport.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnExport.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnExport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExport.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExport.FlatAppearance.BorderSize = 2;
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.Indigo;
+            this.btnExport.Location = new System.Drawing.Point(1055, 3);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(142, 47);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // dgvDisplay
+            // 
+            this.dgvDisplay.AllowUserToOrderColumns = true;
+            this.dgvDisplay.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvDisplay.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplay.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDate,
+            this.colStartTime,
+            this.colLeavingTime,
+            this.colHoursSpent});
+            this.dgvDisplay.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dgvDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDisplay.Location = new System.Drawing.Point(3, 90);
+            this.dgvDisplay.Name = "dgvDisplay";
+            this.dgvDisplay.RowTemplate.Height = 28;
+            this.dgvDisplay.Size = new System.Drawing.Size(1200, 537);
+            this.dgvDisplay.TabIndex = 3;
+            // 
+            // colDate
+            // 
+            this.colDate.HeaderText = "Date";
+            this.colDate.Name = "colDate";
+            this.colDate.ReadOnly = true;
+            // 
+            // colStartTime
+            // 
+            this.colStartTime.HeaderText = "Start Time";
+            this.colStartTime.Name = "colStartTime";
+            this.colStartTime.ReadOnly = true;
+            // 
+            // colLeavingTime
+            // 
+            this.colLeavingTime.HeaderText = "Leaving Time";
+            this.colLeavingTime.Name = "colLeavingTime";
+            this.colLeavingTime.ReadOnly = true;
+            // 
+            // colHoursSpent
+            // 
+            this.colHoursSpent.HeaderText = "Hours Spent";
+            this.colHoursSpent.Name = "colHoursSpent";
+            this.colHoursSpent.ReadOnly = true;
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(13, 672);
+            this.pbStatus.Maximum = 250;
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(747, 39);
+            this.pbStatus.TabIndex = 4;
+            // 
+            // rbWatchFolder
+            // 
+            this.rbWatchFolder.AutoSize = true;
+            this.rbWatchFolder.BackColor = System.Drawing.Color.Transparent;
+            this.rbWatchFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbWatchFolder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbWatchFolder.Location = new System.Drawing.Point(3, 633);
+            this.rbWatchFolder.Name = "rbWatchFolder";
+            this.rbWatchFolder.Size = new System.Drawing.Size(172, 30);
+            this.rbWatchFolder.TabIndex = 5;
+            this.rbWatchFolder.TabStop = true;
+            this.rbWatchFolder.Text = "Watch folder";
+            this.rbWatchFolder.UseVisualStyleBackColor = false;
+            this.rbWatchFolder.CheckedChanged += new System.EventHandler(this.rbWatchFolder_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel1.BackgroundImage")));
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvDisplay, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.rbWatchFolder, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1206, 725);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.btnExport, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnImport, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 669);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1200, 53);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1200, 81);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(399, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(798, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Person Report";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1206, 725);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pbStatus);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Hive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.DataGridView dgvDisplay;
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.RadioButton rbWatchFolder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStartTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLeavingTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHoursSpent;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ The Hive/The Hive/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+
+namespace The_Hive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FileSystemWatcher fswDirectoryWatcher = null;
+        StreamReader myReader = null;
+        string[] myReadLine;
+        string myLastDate = "", myLeavingTime = "", myStartTime = "", myPerson = "";
+        double mytotalhours = 0;
+
+        //string avgStartTime = "", avgLeavingTime = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fswDirectoryWatcher = new FileSystemWatcher();
+            fswDirectoryWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Attributes;
+
+            fswDirectoryWatcher.Created += new FileSystemEventHandler(fswDirectoryWatcher_Changed);
+            fswDirectoryWatcher.IncludeSubdirectories = true;
+
+            rbWatchFolder.Enabled = false;
+            rbWatchFolder.Visible = false;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            pbStatus.Value = 0;
+
+            try
+            {
+                dgvDisplay.Controls.Clear();
+                dgvDisplay.Refresh();
+
+                using (OpenFileDialog ImportDialog = new OpenFileDialog())
+                {
+                    DialogResult myResult = ImportDialog.ShowDialog();
+
+                    if (myResult == DialogResult.OK)
+                    {
+                        GetDataFromFile(ImportDialog.FileName);
+                    }
+                    else
+                    {
+                        MessageBox.Show("CSV file not imported.\nPlease try again", "Error");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("CSV file not imported.\nPlease close the file you are trying to import and try again", "Error - file open");
+
+            }
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            pbStatus.Value = 0;
+
+            Write_Excel_File(myPerson);
+
+            pbStatus.Value = pbStatus.Maximum;
+        }
+
+        private void rbWatchFolder_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbWatchFolder.Checked)
+            {
+                try
+                {
+                    using (FolderBrowserDialog ImportDialog = new FolderBrowserDialog())
+                    {
+                        DialogResult myResult = ImportDialog.ShowDialog();
+
+                        if (myResult == DialogResult.OK)
+                        {
+                            fswDirectoryWatcher.Path = ImportDialog.SelectedPath;
+
+                            fswDirectoryWatcher.EnableRaisingEvents = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Folder to watch not specified.\nPlease try again", "Error");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Folder to watch not specified.\n{ex.Message}", "Error - file open");
+
+                }
+            }
+            else
+            {
+                fswDirectoryWatcher.EnableRaisingEvents = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImport_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] myFile = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                GetDataFromFile(myFile[0]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error - Droping file");
+            }
+            
+        }
+
+        private void btnImport_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void fswDirectoryWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            GetDataFromFile(e.FullPath);
+
+        }
+        private void GetDataFromFile(string myFilePath)
+        {
+            myStartTime = "";
+            myLastDate = "";
+            myLeavingTime = "";
+            mytotalhours = 0;
+
+            myReader = new StreamReader(myFilePath);
+
+            myReader.ReadLine();
+            myReader.ReadLine();
+            myPerson = myReader.ReadLine().Split(',')[0].Trim('"').Replace(':', '-');
+            myReader.ReadLine();
+            myReader.ReadLine();
+
+            while (myReader.EndOfStream == false)
+            {
+                myReadLine = myReader.ReadLine().Split(',');
+
+                if (myLastDate == "")
+                {
+                    myLastDate = myReadLine[0].Trim('"');
+                    myStartTime = myReadLine[1].Trim('"');
+                }
+                else if (myLastDate == myReadLine[0].Trim('"'))
+                {
+                    myLeavingTime = myReadLine[1].Trim('"');
+                }
+                else if (myLastDate != myReadLine[0].Trim('"'))
+                {
+                    dgvDisplay.Rows.Add(myLastDate.Substring(3), myStartTime, myLeavingTime, (Convert.ToDateTime(myLeavingTime) - Convert.ToDateTime(myStartTime)).TotalHours.ToString("f1"));
+                    myLastDate = "";
+                    mytotalhours += (Convert.ToDateTime(myLeavingTime) - Convert.ToDateTime(myStartTime)).TotalHours;
+                }
+
+                try
+                {
+                    if (pbStatus.Value < pbStatus.Maximum)
+                        pbStatus.Increment(1);
+                }
+                catch
+                {
+
+                }
+                
+            }
+
+            dgvDisplay.Rows.Add("", "", "", "Total Hours");
+            dgvDisplay.Rows.Add("", "", "", mytotalhours.ToString("f1"));
+            myReader.Close();
+
+            try
+            {
+
+                pbStatus.Value = pbStatus.Maximum;
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void Write_Excel_File(string myPersonName)
+        {
+            try
+            {
+                pbStatus.Value = 0;
+            }
+            catch
+            {
+
+            }
+
+            _Application myApp = new Microsoft.Office.Interop.Excel.Application();
+            _Workbook myWorkbook = myApp.Workbooks.Add(Type.Missing);
+            _Worksheet myWorksheet = null;
+
+            myWorksheet = myWorkbook.Sheets["Sheet1"];
+            myWorksheet = myWorkbook.ActiveSheet;
+            myWorksheet.Name = myPersonName.Substring(10).Split('(')[0].Trim(' ');
+
+            myApp.Visible = true;
+
+            for (int i = 1; i < dgvDisplay.Columns.Count + 1; i++)
+            {
+                myWorksheet.Cells[1, i] = dgvDisplay.Columns[i - 1].HeaderText;
+            }
+
+            for (int r = 0; r < dgvDisplay.Rows.Count; r++)
+            {
+                for (int c = 0; c < dgvDisplay.Columns.Count; c++)
+                {
+                    try
+                    {
+                        myWorksheet.Cells[r + 2, c + 1] = dgvDisplay.Rows[r].Cells[c].Value.ToString();
+
+                        if (pbStatus.Value < 100)
+                        {
+                            pbStatus.Increment(1);
+                        }
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var saveFileDialoge = new SaveFileDialog();
+            saveFileDialoge.FileName = myPerson;
+            saveFileDialoge.DefaultExt = ".xlsx";
+
+            if (saveFileDialoge.ShowDialog() == DialogResult.OK)
+            {
+                myWorkbook.SaveAs(saveFileDialoge.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            }
+
+            myWorkbook.Close();
+            myApp.Visible = false;
+
+            try
+            {
+                pbStatus.Value = pbStatus.Maximum;
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
